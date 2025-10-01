cb193b2479a537263eaacc16e48e34b411e83032
MiguelAngel313 <rmiguelangel222@gmail.com>
2025-10-01 13:53:21 +0200

    Correción de error de directorio y solución de error de representación en pantalla de los datos(continuar dando formato a los datos pasados en Entrada y agregar datos restantes.)

--- src/main/java/es/albarregas/controllers/Entrada.java
+++ src/main/java/es/albarregas/controllers/Entrada.java
@@ -33,24 +33,14 @@ public class Entrada extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet Entrada</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Entrada at " + request.getContextPath() + "</h1>");
-            out.println("<h2>M&eacute;todo de llamada es " + request.getMethod() + "</h2>");
-            Enumeration<String> parametros=request.getParameterNames();
-            out.println("<ul>");
-            
-            while (parametros.hasMoreElements()) {                
-                String nombre =parametros.nextElement();
-                out.println("<li>"+nombre+" con valor "+request.getParameter(nombre)+"</li>");
-            }
-            out.print("</ul>");
-            out.println("<a href=\".\">Volver</a>");
+            out.println("Nombre: "+request.getParameter("nombre"));
+            System.out.println("<a href=\".\">Volver</a>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -70,7 +60,6 @@ public class Entrada extends HttpServlet {
 
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -79,10 +68,6 @@ public class Entrada extends HttpServlet {
             out.println("<body>");
             out.println("<h1>Servlet Entrada at " + request.getContextPath() + "</h1>");
             out.println("<h2>M&eacute;todo de llamada es " + request.getMethod() + "</h2>");
-            String para1 = request.getParameter("param1");
-            String para2 = request.getParameter("param2");
-            out.println("Parametro1: " + para1);
-            out.println("Parametro2: " + para2);
             out.println("<a href=\".\">Volver</a>");
             out.println("</body>");
             out.println("</html>");
